c1905aab9c953f6d34b3b3e85b2392a4d7cd5c5a
unseen084 <mushfiqur696@gmail.com>
2022-02-28 06:22:15 +0600

    Add projects

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside th`e project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,5 +3,5 @@ from .models import Project
 
 
 def home(request):
-    projects = Project.objects.all()
+    projects = Project.objects.order_by('-id')[:3]
     return render(request, 'portfolio/home.html', {'projects': projects})
